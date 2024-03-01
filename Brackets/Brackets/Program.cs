e9bba4d017a0c5245939e0d9fa14fb8d2ed86994
Timofey <shabalin.timon@gmail.com>
2024-03-01 23:40:06 +0300

    Brackets

---
+++ Brackets/Brackets/Program.cs
@@ -0,0 +1,71 @@
+﻿using Brackets;
+
+static bool CheckString(string str)
+{
+    var stack = new StackList<char>();
+    foreach (char ch in str)
+    {
+        if (ch == '(' || ch == '{' || ch == '[')
+            stack.Push(ch);
+        else
+        {
+            if (ch == ')')
+            {
+                if (stack.IsEmpty() || stack.Pop() != '(')
+                    return false;
+
+            }
+            else if (ch == '}')
+            {
+                if (stack.IsEmpty() || stack.Pop() != '{')
+                    return false;
+
+            }
+            else if (ch == ']')
+            {
+                if (stack.IsEmpty() || stack.Pop() != '[')
+                    return false;
+
+            }
+        }
+    }
+    return stack.IsEmpty();
+}
+
+static bool CheckString2(string str)
+{
+    var stack = new StackOnArray<char>();
+    foreach (char ch in str)
+    {
+        if (ch == '(' || ch == '{' || ch == '[')
+            stack.Push(ch);
+        else
+        {
+            if (ch == ')')
+            {
+                if (stack.IsEmpty() || stack.Pop() != '(')
+                    return false;
+
+            }
+            else if (ch == '}')
+            {
+                if (stack.IsEmpty() || stack.Pop() != '{')
+                    return false;
+
+            }
+            else if (ch == ']')
+            {
+                if (stack.IsEmpty() || stack.Pop() != '[')
+                    return false;
+
+            }
+        }
+    }
+    return stack.IsEmpty();
+}
+Console.WriteLine(CheckString("{[()]}"));
+Console.WriteLine(CheckString("{}[]()"));
+Console.WriteLine(CheckString("{}[]("));
+Console.WriteLine(CheckString("{}][()"));
+Console.WriteLine(CheckString("{[}]"));
+Console.WriteLine(CheckString2("{[()]}"));
---
+++ Brackets/Brackets/StackOnArray.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brackets
+{
+    public class StackOnArray<T>
+    {
+        private T[] array;
+        private const int capacity = 4;
+        private int size;
+
+        public StackOnArray()
+        {
+            size = 0;
+            array = new T[capacity];
+        }
+
+        public bool IsEmpty()
+        {
+            return size == 0;
+        }
+
+        public T Pop()
+        {
+            if (size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return array[size--];
+        }
+
+        public void Push(T newElement)
+        {
+            if (size == array.Length)
+            {
+                T[] newArray = new T[2 * array.Length];
+                Array.Copy(array, newArray, size);
+                array = newArray;
+            }
+            array[++size] = newElement;
+        }
+    }
+}
