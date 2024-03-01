d74abde9080b70b02e8c9a343be9cb6023cd232c
Timofey <shabalin.timon@gmail.com>
2024-03-01 23:49:35 +0300

    OL

---
+++ OrderedList/OrderedList/Elem.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderedList
+{
+    public class MyLinkedList<T>
+    {
+        public Elem<T> First { get; set; }
+
+
+        public void AddFirst(T item)
+        {
+            Elem<T> newElem = new Elem<T>();
+            newElem.Info = item;
+            newElem.Next = First;
+
+            First = newElem;
+        }
+
+        public void AddLast(T item)
+        {
+            if (First == null)
+            {
+                AddFirst(item);
+                return;
+            }
+            var el = First;
+            while (el.Next != null)
+                el = el.Next;
+
+            el.Next = new Elem<T> { Info = item };
+        }
+
+        public void RemoveValue(T x)
+        {
+            while (First != null && First.Info.Equals(x))
+            {
+                First = First.Next;
+            }
+            if (First == null)
+            {
+                return;
+            }
+
+            var elem = First;
+            while (elem.Next != null)
+            {
+                if (elem.Next.Info.Equals(x))
+                    elem.Next = elem.Next.Next;
+                else
+                    elem = elem.Next;
+            }
+        }
+
+        public void RemoveAtIndex(int k)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+        public bool CountOfValue(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            var el = First;
+            while (el != null)
+            {
+                sb.Append($"{el.Info} -> ");
+                el = el.Next;
+            }
+            sb.Append("null");
+            return sb.ToString();
+        }
+    }
+
+    public class Elem<T>
+    {
+        public T Info { get; set; }
+        public Elem<T> Next { get; set; }
+    }
+}
---
+++ OrderedList/OrderedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using OrderedList;
+
+Random r = new Random();
+var l = new OrderedList<Product>();
+
+
+l.Add(new Product() { Id = 1, Name = "Product 1", Price = 100 });
+l.Add(new Product() { Id = 2, Name = "Product 2", Price = 10 });
+l.Add(new Product() { Id = 3, Name = "Product 3", Price = 50 });
+l.Add(new Product() { Id = 4, Name = "Product 4", Price = 110 });
+l.Add(new Product() { Id = 5, Name = "Product 5", Price = 5 });
+//l.Add(new Product() { Id = 3, Name = "Product 3", Price = 50 });
+
+//l.Add(new Product() { Id = 5, Name = "Product 5", Price = 5 });
+
+
+Console.WriteLine(l);
+Console.WriteLine("*******************************");
+
+var l1 = new OrderedList<Product>();
+
+l1.Add(new Product() { Id = 1, Name = "Product 1", Price = 1 });
+l1.Add(new Product() { Id = 2, Name = "Product 2", Price = 11 });
+l1.Add(new Product() { Id = 3, Name = "Product 3", Price = 55 });
+l1.Add(new Product() { Id = 4, Name = "Product 4", Price = 99 });
+l1.Add(new Product() { Id = 5, Name = "Product 5", Price = 200 });
+
+Console.WriteLine(l1);
+Console.WriteLine("*******************************");
