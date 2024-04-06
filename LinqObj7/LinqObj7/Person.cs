7dd058f7f69cc78b2493ebf02e1ae99c4f3dc5b6
ShabalinTK <shabalin.timon@gmail.com>
2024-04-06 23:59:45 +0300

    LO7

---
+++ LinqObj7/LinqObj7/Person.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace LinqObj7
+{
+    public class Person
+    {
+        public int ClientCode { get; set; }
+        public List<int> Year { get; set; }
+        public Dictionary<int, List<int>> Month { get; set; }
+        public Dictionary<int, Dictionary<int, int>> Hours { get; set; }
+
+        public Person()
+        {
+            Year = new List<int>();
+            Month = new Dictionary<int, List<int>>();
+            Hours = new Dictionary<int, Dictionary<int, int>>();
+        }
+    }
+}
