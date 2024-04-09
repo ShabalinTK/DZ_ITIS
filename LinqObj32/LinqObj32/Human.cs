3afb60c9dddd76320c00047d716b3aac945e9a2e
ShabalinTK <shabalin.timon@gmail.com>
2024-04-09 19:32:46 +0300

    LO32

---
+++ LinqObj32/LinqObj32/Human.cs
@@ -0,0 +1,15 @@
+﻿namespace LinqObj29
+{
+    public class Human
+    {
+        public int Number { get; set; }
+        public string Surname { get; set; }
+        public double Sum { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Number: {Number}, Surname: {Surname}, Sum: {Sum}";
+        }
+    }
+}
