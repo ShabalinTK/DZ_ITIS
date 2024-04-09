ab224eec02d526743238b5f47d55b2c3c12d11ec
ShabalinTK <shabalin.timon@gmail.com>
2024-04-09 10:17:39 +0300

    LO40

---
+++ LinqObj40/LinqObj40/Azs.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace LinqObj40
+{
+    public class Azs
+    {
+        public string Comp { get; set; }
+
+        public Dictionary<string, Dictionary<int, List<int>>> Str { get; set; }
+
+        public Dictionary<int, List<int>> Number { get; set; }
+
+        public List<int> Sum { get; set; }
+
+        public override string ToString()
+        {
+            return $"Comp: {Comp}, Str: {Str}, Number: {Number}, Sum: {Sum}";
+        }
+    }
+}
---
+++ LinqObj40/LinqObj40/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace LinqObj40
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class InfoAzs
+    {
+        public void InfoAzs()
+        {
+            var groups = new List<Azs>;
+            groups.Add()
+        }
+        private Azs azs1
+        {
+            return new Azs()
+        {
+
+        }
+
+            grup.Add(new Azs
+            {
+                Comp = "tatneft",
+                Sum = new List<int> { 4200, 10000},
+                Number = new Dictionary<int, List<int>>
+                {
+                    {92, new List<int> {4200} },
+                    {95, new List<int> {10000} }
+                },
+
+                Str = new Dictionary<string, Dictionary<int, List<int>>>
+                {
+                    
+                    {
+                        "1/1", new Dictionary<int, List<int>>
+                        {
+                            {92, new List<int> {4200 } },
+                            {95, new List<int> {10000} }
+                        }
+                    },
+                    {
+                        "2/1", new Dictionary<int, List<int>>
+                        {
+                            {92, new List<int> {4200} },
+                            {95, new List<int> {10000} }
+                        }
+                    }
+                }
+            });
+        }
+
+        public void Str2()
+        {
+            var grup = new List<Azs>();
+
+            grup.Add(new Azs
+            {
+                Comp = "RosNeft",
+                Sum = new List<int> { 5700, 9800 },
+                Number = new Dictionary<int, List<int>>
+                {
+                    {92, new List<int> {5700 } },
+                    {95, new List<int> {9800} }
+                },
+
+                Str = new Dictionary<string, Dictionary<int, List<int>>>
+                {
+
+                    {
+                        "1/1", new Dictionary<int, List<int>>
+                        {
+                            {92, new List<int> { 5700 } },
+                            {95, new List<int> { 9800 } }
+                        }
+                    },
+                    {
+                        "2/1", new Dictionary<int, List<int>>
+                        {
+                            {92, new List<int> { 5700 } },
+                            {95, new List<int> { 9800 } }
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
